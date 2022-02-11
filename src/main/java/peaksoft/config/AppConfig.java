ee275834c09ccb3af6f5fe8535cc04bca6a9f3ae
User <trrioiw>
2022-02-11 19:23:18 +0600

    test

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,23 @@
 package peaksoft.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
+import peaksoft.model.Animal;
+import peaksoft.model.AnimalsCage;
+import peaksoft.model.Dog;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
+
+
+
+
+
+
 }
